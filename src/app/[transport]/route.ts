684273f22188e184eaf68cd7c6326b8bef76b1bb
8spw <8spw@users.noreply.github.com>
2025-07-25 01:52:13 +0200

    feat: minimal example

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,54 +1,10 @@
-import { createMcpHandler } from "@vercel/mcp-adapter";
-import z from "zod";
+import { createMcpHandler } from "mcp-handler";
+import { z } from "zod";
 
-const handler = createMcpHandler(
-  (server) => {
-    server.tool(
-      "cource_recommendation",
-      "Give a course recommendation to user",
-      {
-        experienceLevel: z.enum(["beginner", "intermediate"]),
-      },
-      ({ experienceLevel }) => ({
-        content: [
-          {
-            type: "text",
-            text: `I would recommend ${
-              experienceLevel === "beginner"
-                ? "Javascript Course"
-                : "Next.js Course"
-            }`,
-          },
-        ],
-      })
-    );
-  },
-  {
-    capabilities: {
-      tools: {
-        cource_recommendation: {
-          description: "Give a course recommendation to user",
-          inputSchema: {
-            type: "object",
-            properties: {
-              experienceLevel: {
-                type: "string",
-                enum: ["beginner", "intermediate"],
-              },
-            },
-            required: ["experienceLevel"],
-          },
-        },
-      },
-    },
-  },
-  {
-    redisUrl: process.env.REDIS_URL_REDIS_URL,
-    sseEndpoint: "/sse",
-    streamableHttpEndpoint: "/mcp",
-    verboseLogs: true,
-    maxDuration: 60,
-  }
-);
+const handler = createMcpHandler((server) => {
+  server.tool("echo", { message: z.string() }, async ({ message }) => ({
+    content: [{ type: "text", text: `Tool echo: ${message}` }],
+  }));
+});
 
-export { handler as POST, handler as GET, handler as DELETE };
+export { handler as GET, handler as POST, handler as DELETE };
