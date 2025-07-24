e4cf63392d98e00002f646b0b87cffe8f8db1369
8spw <8spw@users.noreply.github.com>
2025-07-25 01:06:29 +0200

    fix: redis url

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,54 +1,26 @@
 import { createMcpHandler } from "@vercel/mcp-adapter";
 import z from "zod";
 
-const handler = createMcpHandler(
-  (server) => {
-    server.tool(
-      "cource_recommendation",
-      "Give a course recommendation to user",
-      {
-        experienceLevel: z.enum(["beginner", "intermediate"]),
-      },
-      ({ experienceLevel }) => ({
-        content: [
-          {
-            type: "text",
-            text: `I would recommend ${
-              experienceLevel === "beginner"
-                ? "Javascript Course"
-                : "Next.js Course"
-            }`,
-          },
-        ],
-      })
-    );
-  },
-  {
-    capabilities: {
-      tools: {
-        cource_recommendation: {
-          description: "Give a course recommendation to user",
-          inputSchema: {
-            type: "object",
-            properties: {
-              experienceLevel: {
-                type: "string",
-                enum: ["beginner", "intermediate"],
-              },
-            },
-            required: ["experienceLevel"],
-          },
-        },
-      },
+const handler = createMcpHandler((server) => {
+  server.tool(
+    "cource_recommendation",
+    "Give a course recommendation to user",
+    {
+      experienceLevel: z.enum(["beginner", "intermediate"]),
     },
-  },
-  {
-    redisUrl: process.env.REDIS_URL_REDIS_URL,
-    sseEndpoint: "/sse",
-    streamableHttpEndpoint: "/mcp",
-    verboseLogs: true,
-    maxDuration: 60,
-  }
-);
+    ({ experienceLevel }) => ({
+      content: [
+        {
+          type: "text",
+          text: `I would recommend ${
+            experienceLevel === "beginner"
+              ? "Javascript Course"
+              : "Next.js Course"
+          }`,
+        },
+      ],
+    })
+  );
+});
 
 export { handler as POST, handler as GET, handler as DELETE };
