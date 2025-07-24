e0e9bd6f2c020ee0b1c2959a77facf58d7012655
8spw <8spw@users.noreply.github.com>
2025-07-24 20:53:50 +0200

    fix: missing field

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -4,7 +4,7 @@ import z from "zod";
 const handler = createMcpHandler(
   (server) => {
     server.tool(
-      "cource_recommentaion",
+      "cource_recommendation",
       "Give a course recommendation to user",
       {
         experienceLevel: z.enum(["beginner", "intermediate"]),
@@ -13,16 +13,20 @@ const handler = createMcpHandler(
         content: [
           {
             type: "text",
-            content: `I would recommend ${experienceLevel === "beginner" ? "Javascript Course" : "Next.js Course"}`,
+            text: `I would recommend ${
+              experienceLevel === "beginner"
+                ? "Javascript Course"
+                : "Next.js Course"
+            }`,
           },
         ],
-      }),
+      })
     );
   },
   {
     capabilities: {
       tools: {
-        cource_recommentaion: {
+        cource_recommendation: {
           description: "Give a course recommendation to user",
         },
       },
@@ -34,7 +38,7 @@ const handler = createMcpHandler(
     streamableHttpEndpoint: "/mcp",
     verboseLogs: true,
     maxDuration: 60,
-  },
+  }
 );
 
 export { handler as POST, handler as GET, handler as DELETE };
