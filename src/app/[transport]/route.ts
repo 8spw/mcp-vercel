a9de3859844f96652c61915dec692816bfdabdd1
8spw <8spw@users.noreply.github.com>
2025-07-25 01:56:19 +0200

    feat: minimal example

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,10 +1,24 @@
 import { createMcpHandler } from "mcp-handler";
 import { z } from "zod";
 
-const handler = createMcpHandler((server) => {
-  server.tool("echo", { message: z.string() }, async ({ message }) => ({
-    content: [{ type: "text", text: `Tool echo: ${message}` }],
-  }));
-});
+const handler = createMcpHandler(
+  (server) => {
+    server.tool("echo", { message: z.string() }, async ({ message }) => ({
+      content: [{ type: "text", text: `Tool echo: ${message}` }],
+    }));
+  },
+  {
+    capabilities: {
+      tools: {
+        echo: {
+          description: "Echo a message",
+        },
+      },
+    },
+  },
+  {
+    redisUrl: process.env.REDIS_URL_REDIS_URL,
+  }
+);
 
 export { handler as GET, handler as POST, handler as DELETE };
