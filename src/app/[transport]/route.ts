ee2fbde3d536f149876f1e8221fd68c01523675b
8spw <8spw@users.noreply.github.com>
2025-07-25 01:20:09 +0200

    fix: redis url

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,26 +1,54 @@
 import { createMcpHandler } from "@vercel/mcp-adapter";
 import z from "zod";
 
-const handler = createMcpHandler((server) => {
-  server.tool(
-    "cource_recommendation",
-    "Give a course recommendation to user",
-    {
-      experienceLevel: z.enum(["beginner", "intermediate"]),
-    },
-    ({ experienceLevel }) => ({
-      content: [
-        {
-          type: "text",
-          text: `I would recommend ${
-            experienceLevel === "beginner"
-              ? "Javascript Course"
-              : "Next.js Course"
-          }`,
+const handler = createMcpHandler(
+  (server) => {
+    server.tool(
+      "cource_recommendation",
+      "Give a course recommendation to user",
+      {
+        experienceLevel: z.enum(["beginner", "intermediate"]),
+      },
+      ({ experienceLevel }) => ({
+        content: [
+          {
+            type: "text",
+            text: `I would recommend ${
+              experienceLevel === "beginner"
+                ? "Javascript Course"
+                : "Next.js Course"
+            }`,
+          },
+        ],
+      })
+    );
+  },
+  {
+    capabilities: {
+      tools: {
+        cource_recommendation: {
+          description: "Give a course recommendation to user",
+          inputSchema: {
+            type: "object",
+            properties: {
+              experienceLevel: {
+                type: "string",
+                enum: ["beginner", "intermediate"],
+              },
+            },
+            required: ["experienceLevel"],
+          },
         },
-      ],
-    })
-  );
-});
+      },
+    },
+  },
+  {
+    redisUrl: process.env.REDIS_URL_REDIS_URL,
+    sseEndpoint: "/sse",
+    streamableHttpEndpoint: "/mcp",
+    verboseLogs: true,
+    maxDuration: 60,
+  }
+);
 
 export { handler as POST, handler as GET, handler as DELETE };
