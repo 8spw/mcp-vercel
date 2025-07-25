093384db7c1498ab62685300bc28161fb8909eae
8spw <8spw@users.noreply.github.com>
2025-07-25 15:23:18 +0200

    feat: new redis url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold font-mono">MCP Server</h1>
       </div>
