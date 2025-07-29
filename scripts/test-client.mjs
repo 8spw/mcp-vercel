4d510870ef7e24a0ecae10511f8481fe2413e2e6
8spw <8spw@users.noreply.github.com>
2025-07-29 02:18:00 +0200

    fix: kmasd

--- scripts/test-client.mjs
+++ scripts/test-client.mjs
@@ -2,6 +2,7 @@ import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
 
 const origin = "https://mcp-vercel-six.vercel.app";
+// const origin = "http://localhost:3000";
 
 async function main() {
   const transport = new SSEClientTransport(new URL(`${origin}/sse`));
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid">
+    <div className="grid p-4">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold font-mono">MCP Server</h1>
       </div>
